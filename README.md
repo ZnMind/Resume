831a3ea089c5df08211a65753edb81b258fa49a2
DKProtocol <damann1072@gmail.com>
2023-08-09 10:57:29 -0500

    Reworking some things

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is my custom resume built entirely in React!
 
 Main file can be found in [/src/App.js](/src/App.js)
 
-[Check it out!](https://www.linkedin.com/in/mann-dan/overlay/1635524247787/single-media-viewer/?profileId=ACoAABSqCzEBL7Gr_Y2QLlkupKXzkWbHCpW6yZc)
+[Check it out!](https://www.linkedin.com/in/mann-dan/overlay/1635535830984/single-media-viewer/?profileId=ACoAABSqCzEBL7Gr_Y2QLlkupKXzkWbHCpW6yZc)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactToPrint from 'react-to-print';
 import image from './img/imgHelper';
 import './App.css';
 
 const FancyLine = ({ color1, color2, color3, size }) => {
   return (
-    <div style={{ display: 'flex', marginTop: '1em' }}>
+    <div style={{ display: 'flex', marginTop: '0.5em' }}>
       <div style={{ backgroundColor: color1, width: '33.33%', height: size, borderTopLeftRadius: '2px', borderBottomLeftRadius: '2px' }}></div>
       <div style={{ backgroundColor: color2, width: '33.33%', height: size, borderTopLeftRadius: '1px', borderBottomLeftRadius: '1px' }}></div>
       <div style={{ backgroundColor: color3, width: '33.33%', height: size, borderTopRightRadius: '2px', borderBottomRightRadius: '2px' }}></div>
@@ -34,10 +34,10 @@ const ContactBox = () => {
 
 const SkillBox = () => {
   const skillset = [
-    'JavaScript', 'HTML5', 'CSS', 'React.js',
-    'Redux', 'Node.js', 'Express', 'MySQL', 
-    'MongoDB', 'GIT', 'Google Apps Script', 'Next.js', 
-    'UI Design', 'Bootstrap', 'Agile Methodologies', 'Problem Solving'
+    'JavaScript', 'HTML5', 'CSS', 'React',
+    'Redux', 'Node.js', 'Express', 'MySQL',
+    'MongoDB', 'WordPress', 'GIT', 'Next.js',
+    'UI Design', 'Bootstrap', 'Agile Methodologies'
   ];
 
   return (
@@ -47,32 +47,41 @@ const SkillBox = () => {
         <h3 style={{ width: '100%', marginTop: '0', marginBottom: '0', color: '#373a41', marginLeft: '-0' }}>SKILLS</h3>
       </div>
       {skillset.map((data, index) => (
-        <p key={index} style={{ width: '25%' }}>{data}</p>
+        <p key={index} style={{ width: '20%' }}>{data}</p>
       ))}
     </div>
   )
 }
 
 const WorkBox = () => {
-  const titles = ['Web Developer', 'Full Stack Web Developer', 'Annuities Account Representative'];
-  const companies = ['Innov8tive Strategies, LLC', 'Innovate Birmingham', 'Tata Consultancy Services'];
-  const dates = ['07/2022 - Present', '12/2021 - 05/2022', '10/2020 - 12/2021'];
+  const titles = ['Software Developer (Contract)', 'Web Developer (Contract)', 'Full Stack Web Developer'];
+  const companies = ['Relay', 'Innov8tive Strategies, LLC', 'Innovate Birmingham'];
+  const dates = ['06/2023 - Present', '07/2022 - 06/2023', '12/2021 - 05/2022'];
   const bullets = [
     [
-      <small>Coordinating with CEO to improve company website <b>UI/UX design</b></small>,
-      <small>Implemented <b>RESTful API</b> for live Instagram feed</small>,
-      <small>Driving greater traffic to website through <b>SEO</b> best practices</small>,
-      <small>Consulting with non-profit clients to grow their website and, through that, their impact in their community</small>
+      <small>
+        Enhanced client data monitoring efficiency by integrating the <b>Front-End</b> website with the <b>Back-End</b> CRM system, and
+        implemented seamless data synchronization, resulting in a marked increase in data tracking and accuracy.
+      </small>,
+      <small>Increased <b>WordPress</b> site indexability by a significant margin through plugins such as <b>YoastSEO</b></small>,
+      <small>
+        Built a webscraping software tool using <b>Node.js</b>, <b>Express</b>, and <b>Cheerio</b> to effectively monitor site structure
+        and ensure all <b>SEO</b> best practices were met
+      </small>
     ],
     [
-      <small>Web development boot camp to continue growth and challenge myself as a software developer</small>,
-      <small>Actively developing <b>Front-End</b> applications using <b>React.js</b></small>,
-      <small>Building <b>API</b> and <b>Back-End</b> architecture with <b>Node.js</b> and <b>Express</b></small>,
-      <small>Designing relational databases with <b>MySQL</b> to handle robust data sets</small>
+      <small>Collaborated with the CEO to improve company website <b>UI/UX design</b>, performance, and reliability</small>,
+      <small>Designed and implemented an <b>API</b> using <b>RESTful</b> services to enable real-time integration of a live Instagram feed</small>,
+      <small>Improved website traffic through identifying and applying <b>SEO</b> best practices</small>,
+      <small>Consulted with non-profit clients to grow their own web presence and, through that, their impact in the community</small>
     ],
     [
-      <small>Third Party Administrator to a multi-billion dollar company providing in-depth knowledge on our retirement products and investment offerings</small>,
-      <small>Updated and oversaw client information across several departments and ensured its accuracy in our <b>CRM</b> software</small>
+      <small>Web development boot camp to grow my <b>coding</b> abilities and challenge myself as a <b>JavaScript</b> developer</small>,
+      <small>Actively developed <b>Front-End</b> applications using <b>React</b> with features such as <b>React Hooks</b> and <b>React Router</b></small>,
+      <small>
+        Built <b>API</b> and <b>Back-End</b> architecture with <b>Node.js</b> and <b>Express</b>,
+        as well as designed a relational database with <b>MySQL</b> to handle robust data sets
+      </small>
     ]
   ];
 
@@ -86,7 +95,7 @@ const WorkBox = () => {
       <div style={{ display: 'flex' }}>
         <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '0.55em', marginTop: '0.5em' }}>
           <div className='circle' style={{ marginTop: '0' }}></div>
-          <div className='vert'></div>
+          <div className='vert' style={{ height: 'calc(118px + 1.5em)' }}></div>
           <div className='circle'></div>
           <div className='vert'></div>
           <div className='circle'></div>
@@ -120,7 +129,7 @@ const EducationBox = () => {
   const bullets = [
     <small>Project based learning through coursework with a strong emphasis on <b>leadership</b> and <b>teamwork</b> in a group setting</small>,
     <small>Relevant coursework: Information systems design, Object-Oriented Programming, Data Structures and Algorithms,
-      Software Development, Computer Science, Database Management Systems</small>
+      Software Development, Computer Science, Database Management Systems, CI/CD, Git, Responsive Web Design</small>
   ];
 
   return (
@@ -152,6 +161,50 @@ const EducationBox = () => {
   )
 }
 
+/** 
+ * List of key words/phrases hidden behind the resume to attempt to get past ATS and have a real person see the resume.
+ * Seems slightly unethical so won't be included. Fun to experiment, though.
+ */
+const ATSKeywords = () => {
+  let keys = [
+    'javascript', 'javascript', 'javascript', 'javascript', 'javascript', 'javascript', 'javascript', 'javascript',
+    'front-end', 'front-end', 'front-end', 'back-end', 'back-end', 'back-end',
+    'Front-end development ', 'Back-end development ', 'Full-stack development ', 'Agile ', 'Scrum ', 'Continuous Integration ',
+    'Continuous Deployment ', 'DevOps ', 'Git ', 'Docker ', 'Windows ', 'Object-Oriented Programming ', 'Amazon Web Services ',
+    'Data Structures and Algorithms ', 'Problem Solving ', 'Analytical Skills ', 'Windows ', 'MacOS ', 'WordPress ', 'React Hooks ',
+    'React Router ', 'Component-Based Architecture ', 'Single Page Applications (SPAs) ', 'Responsive Web Design ', 'Webpack ',
+    'Babel ', 'Version Control ', 'ES6/ES7 ', 'NPM/Yarn ', 'Cross-Browser Compatibility ', 'Jest ', 'Server-Side Rendering (SSR) ',
+    'GraphQL '
+  ];
+
+  return (
+    <div className='ats'>
+      {keys.map((data, index) => (
+        <div key={index} style={{ marginRight: '0.5em' }}>{data}</div>
+      ))}
+    </div>
+  )
+}
+
+// Just a test to see what would happen
+const Hover = () => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div className='hover' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+      <b style={{ fontSize: 'large', color: 'white' }}>ⓘ</b>
+      {
+        hover
+          ? <div className='hover-box'>
+            <p>I wrote and designed this resume entirely from scratch in React!</p>
+            <p><a href='https://github.com/ZnMind/Resume' target='_blank' rel='noreferrer' style={{ color: 'steelblue' }}>Link</a></p>
+          </div>
+          : ""
+      }
+    </div>
+  )
+}
+
 class App extends React.Component {
   render() {
     return (
@@ -160,10 +213,11 @@ class App extends React.Component {
           <h1 className='name' style={{ marginTop: '0.25em' }}>Daniel Mann</h1>
           <h3 className='title'>Software Developer</h3>
           <p className='description'>
-            I am a motivated individual with a passion for software development,
-            specifically the challenge it provides and the reward of solving complex problems.
-            I enjoy thinking outside the box and creating unique solutions that align with my strengths,
-            while constantly learning something new every day.
+            Innovative and results-driven software developer with an enthusiasm for tackling complex challenges and crafting elegant solutions.
+            With a passion for continuous learning,
+            I'm eager to embrace the newest technologies and industry best practices to continue personal growth and deliver high-quality code.
+            Leveraging a strong foundation in problem-solving and a creative mindset,
+            I thrive in collaborative environments and excel at developing user-centric applications.
           </p>
 
           <ContactBox />
@@ -188,7 +242,10 @@ class App extends React.Component {
 
           <EducationBox />
 
+          {/* <Hover /> */}
         </div>
+
+        {/* <ATSKeywords /> */}
       </div>
     );
   }
@@ -199,12 +256,12 @@ const AppToPrint = () => {
 
   return (
     <div>
+      <App ref={el => (componentRef = el)} />
+
       <ReactToPrint
-        trigger={() => <button>{`Print as PDF :)`}</button>}
+        trigger={() => <button style={{ marginTop: '1em' }}>{`Print as PDF :)`}</button>}
         content={() => componentRef}
       />
-
-      <App ref={el => (componentRef = el)} />
     </div>
   )
 }
